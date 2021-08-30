ce7778677c049bae76c6b5eca53647dea835cdb1
ELOATS <2514307815@qq.com>
2021-08-30 23:12:33 +0800

    model and error

---
+++ account/model/errors.go
@@ -0,0 +1,97 @@
+package model
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+// Type holds a type string and integer code for the error
+type Type string
+
+const (
+	Authorization   Type = "AUTHORIZATION"
+	BadRequest      Type = "BADREQUEST" // 409
+	Conflict        Type = "CONFLICT"
+	Internal        Type = "INTERNAL" // 500
+	NotFound        Type = "NOTFOUND"
+	PayloadTooLarge Type = "PAYLOADTOOLARGE" // 413
+)
+
+type Error struct {
+	Type    Type   `json:"type"`
+	Message string `json:"message"`
+}
+
+func (e *Error) Error() string {
+	return e.Message
+}
+
+func (e *Error) Status() int {
+	switch e.Type {
+	case Authorization:
+		return http.StatusUnauthorized
+	case BadRequest:
+		return http.StatusBadRequest
+	case Conflict:
+		return http.StatusConflict
+	case Internal:
+		return http.StatusInternalServerError
+	case NotFound:
+		return http.StatusNotFound
+	case PayloadTooLarge:
+		return http.StatusRequestEntityTooLarge
+	default:
+		return http.StatusInternalServerError
+	}
+}
+
+func Status(err error) int {
+	var e *Error
+	if errors.As(err, &e) {
+		return e.Status()
+	}
+	return http.StatusInternalServerError
+}
+
+func NewAuthorization(reason string) *Error {
+	return &Error{
+		Type:    Authorization,
+		Message: reason,
+	}
+}
+
+func NewBadRequest(reason string) *Error {
+	return &Error{
+		Type:    BadRequest,
+		Message: fmt.Sprintf("Bad request. Reason: %v", reason),
+	}
+}
+
+func NewConflict(name, value string) *Error {
+	return &Error{
+		Type:    Conflict,
+		Message: fmt.Sprintf("resource: %v with value: %v already exist.", name, value),
+	}
+}
+
+func NewInternal() *Error {
+	return &Error{
+		Type:    Internal,
+		Message: fmt.Sprintf("Internal server error."),
+	}
+}
+
+func NewNotFound(name, value string) *Error {
+	return &Error{
+		Type:    NotFound,
+		Message: fmt.Sprintf("resource: %v with value: %v not found", name, value),
+	}
+}
+
+func NewPayloadTooLarge(maxBodySize, contentLength int64) *Error {
+	return &Error{
+		Type:    PayloadTooLarge,
+		Message: fmt.Sprintf("Max payload size of %v exceeded. Actual payload size: %v", maxBodySize, contentLength),
+	}
+}
